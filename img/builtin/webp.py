d587d4087d9d8578d1effd4170e22ffa51db73d2
liedou <sdk.liedou@gmail.com>
2024-06-18 00:16:44 +0800

    Add RSS

--- img/builtin/webp.py
+++ img/builtin/webp.py
@@ -3,8 +3,8 @@ import os
 dir = os.listdir()
 
 for each in dir:
-    if each != 'webp.py':
-        os.system(f'ffmpeg -i {each} {each[:-3]}webp')
+    if each != 'webp.py' and each[-4:] != 'webp':
+        os.system(f'ffmpeg -i {each} {each.split('.')[0]}.webp')
         os.remove(each)
-
+# os.system('pause')
 print('Success!')    
